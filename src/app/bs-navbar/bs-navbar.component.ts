4803924172c288c49f368695aed1a43ebf2ed2de
Shady A. Abdulhamid <SAMCS224@hotmail.com>
2017-10-05 17:26:37 +0200

    remove explicit subbscribtion of user observable.

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,16 +1,17 @@
 import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-user: firebase.User;
+user$: Observable<firebase.User>;
   constructor(private afAuth: AngularFireAuth) { 
-    afAuth.authState.subscribe(user => this.user= user);
-  }
+    this.user$ = afAuth.authState;
+  } 
 
   logout = () => this.afAuth.auth.signOut();
 
